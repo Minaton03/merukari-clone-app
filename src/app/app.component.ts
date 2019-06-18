a93ba10381ff8f110cd120926d64fb5d54dfc265
Minato <m-yamauchi@taisei-eng.co.jp>
2019-06-19 06:02:17 +0900

    [add]トップ画面。component作成

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+const PRODUCT: product[] = [
+{
+  productName: 'Tシャツ', exhibitor: '出品者A', category: '洋服', brand: 'mina-to', size: 'M',state: 'やや使用感あり', deliveryFee: '200', shippingMethod: 'ゆうパック', region: '東京' , scheduledShippingDate: '3日'},
+  {productName: 'Tシャツ', exhibitor: '出品者A', category: '洋服', brand: 'mina-to', size: 'M',state: 'やや使用感あり', deliveryFee: '200', shippingMethod: 'ゆうパック', region: '東京' , scheduledShippingDate: '3日'},
+  {productName: 'Tシャツ', exhibitor: '出品者A', category: '洋服', brand: 'mina-to', size: 'M',state: 'やや使用感あり', deliveryFee: '200', shippingMethod: 'ゆうパック', region: '東京' , scheduledShippingDate: '3日'},
+  {productName: 'Tシャツ', exhibitor: '出品者A', category: '洋服', brand: 'mina-to', size: 'M',state: 'やや使用感あり', deliveryFee: '200', shippingMethod: 'ゆうパック', region: '東京' , scheduledShippingDate: '3日'},
+  {productName: 'Tシャツ', exhibitor: '出品者A', category: '洋服', brand: 'mina-to', size: 'M',state: 'やや使用感あり', deliveryFee: '200', shippingMethod: 'ゆうパック', region: '東京' , scheduledShippingDate: '3日'},
+  {productName: 'Tシャツ', exhibitor: '出品者A', category: '洋服', brand: 'mina-to', size: 'M',state: 'やや使用感あり', deliveryFee: '200', shippingMethod: 'ゆうパック', region: '東京' , scheduledShippingDate: '3日'},
+];
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'merukari-clone-app';
+  product = PRODUCT;
 }
