8202dac88fff960285f5c6083041af1435edcc73
Minato <m-yamauchi@taisei-eng.co.jp>
2019-06-19 06:46:09 +0900

    [add]商品の出品データのモック作成

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
+import { product } from './class/product';
 
 const PRODUCT: product[] = [
 {
-  productName: 'Tシャツ', exhibitor: '出品者A', category: '洋服', brand: 'mina-to', size: 'M',state: 'やや使用感あり', deliveryFee: '200', shippingMethod: 'ゆうパック', region: '東京' , scheduledShippingDate: '3日'},
-  {productName: 'Tシャツ', exhibitor: '出品者A', category: '洋服', brand: 'mina-to', size: 'M',state: 'やや使用感あり', deliveryFee: '200', shippingMethod: 'ゆうパック', region: '東京' , scheduledShippingDate: '3日'},
-  {productName: 'Tシャツ', exhibitor: '出品者A', category: '洋服', brand: 'mina-to', size: 'M',state: 'やや使用感あり', deliveryFee: '200', shippingMethod: 'ゆうパック', region: '東京' , scheduledShippingDate: '3日'},
-  {productName: 'Tシャツ', exhibitor: '出品者A', category: '洋服', brand: 'mina-to', size: 'M',state: 'やや使用感あり', deliveryFee: '200', shippingMethod: 'ゆうパック', region: '東京' , scheduledShippingDate: '3日'},
-  {productName: 'Tシャツ', exhibitor: '出品者A', category: '洋服', brand: 'mina-to', size: 'M',state: 'やや使用感あり', deliveryFee: '200', shippingMethod: 'ゆうパック', region: '東京' , scheduledShippingDate: '3日'},
-  {productName: 'Tシャツ', exhibitor: '出品者A', category: '洋服', brand: 'mina-to', size: 'M',state: 'やや使用感あり', deliveryFee: '200', shippingMethod: 'ゆうパック', region: '東京' , scheduledShippingDate: '3日'},
+  productName: 'Tシャツ', exhibitor: '出品者A', category: '洋服', brand: 'mina-to', size: 'M',state: 'やや使用感あり', deliveryFee: 200, shippingMethod: 'ゆうパック', region: '東京' , scheduledShippingDate: 3, image: '../assets/img/logo.png'},
+  {productName: 'Tシャツ', exhibitor: '出品者A', category: '洋服', brand: 'mina-to', size: 'M',state: 'やや使用感あり', deliveryFee: 200, shippingMethod: 'ゆうパック', region: '東京' , scheduledShippingDate: 3, image: ''},
+  {productName: 'Tシャツ', exhibitor: '出品者A', category: '洋服', brand: 'mina-to', size: 'M',state: 'やや使用感あり', deliveryFee: 200, shippingMethod: 'ゆうパック', region: '東京' , scheduledShippingDate: 3, image: ''},
+  {productName: 'Tシャツ', exhibitor: '出品者A', category: '洋服', brand: 'mina-to', size: 'M',state: 'やや使用感あり', deliveryFee: 200, shippingMethod: 'ゆうパック', region: '東京' , scheduledShippingDate: 3, image: ''},
+  {productName: 'Tシャツ', exhibitor: '出品者A', category: '洋服', brand: 'mina-to', size: 'M',state: 'やや使用感あり', deliveryFee: 200, shippingMethod: 'ゆうパック', region: '東京' , scheduledShippingDate: 3, image: ''},
+  {productName: 'Tシャツ', exhibitor: '出品者A', category: '洋服', brand: 'mina-to', size: 'M',state: 'やや使用感あり', deliveryFee: 200, shippingMethod: 'ゆうパック', region: '東京' , scheduledShippingDate: 3, image: ''},
 ];
 @Component({
   selector: 'app-root',
---
+++ src/app/class/product.ts
@@ -0,0 +1,13 @@
+export class product {
+    productName: string;
+    exhibitor: string;
+    category: string;
+    brand: string;
+    size: string;
+    state: string;
+    deliveryFee: number;
+    shippingMethod: string;
+    region: string;
+    scheduledShippingDate: number;
+    image: string;
+}
